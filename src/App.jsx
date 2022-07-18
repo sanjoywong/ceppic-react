419eea64e4c5490d147687fcb424532698a2ee25
sanjoy.wong <sanjoy.wong@gmail.com>
2022-07-18 11:45:07 +0200

    affichier les const

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,41 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+const message = <h1>Hello le monde</h1>
+const message2 = (<div>
+  <h3>Block html</h3>
+  <p>Lorem ipsum dolor sit amet consectetur.</p>
+</div>);
+
+const calcul = <h2>Cette operation 2 * 6 vaut {2*6}</h2>;
+const loggedIn = true;
+const voitures = ["Ford","Audi","Honda"];
+const voitures2 =[{"id":1,"nom":"Ford"},{"id":2,"nom":"Audi"},{"id":3,"nom":"Honda"}];
+//comportement
+
+
+//rendu
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {message}
+      {message2}
+      {calcul}
+      {loggedIn && <p>Vous etes connecté</p>}
+      {voitures}
+      <ul>
+        {voitures.map((voiture)=>(
+          <li key={voiture}>{voiture}</li>
+        ))}
+      </ul>
+      <br />
+      <ul>
+        {voitures2.map((voiture)=>(
+          <li key={voiture.id}>{voiture.nom}</li>
+        ))}
+      </ul>
+    
     </div>
   );
 }
